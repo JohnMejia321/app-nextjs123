f50a967b6fab338ee6450c3ac22a4f85962314b2
JohnMejia321 <john.mejias@udea.edu.co>
2025-10-02 21:32:48 -0500

    Add CORS headers manually to auth API route

--- pages/api/auth/[...all].ts
+++ pages/api/auth/[...all].ts
@@ -1,7 +1,31 @@
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from '@/lib/auth';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 // Disallow body parsing, we will parse it manually
 export const config = { api: { bodyParser: false } };
 
-export default toNodeHandler(auth.handler);
+const allowedOrigins = [
+  'https://app-nextjs123-xb82-6sd4gtpy2-johnmejia321s-projects.vercel.app',
+  'https://app-nextjs123-xb82-77p4oh9kv-johnmejia321s-projects.vercel.app',
+  'http://localhost:3000'
+];
+
+const handler = toNodeHandler(auth.handler);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  return handler(req, res);
+};
